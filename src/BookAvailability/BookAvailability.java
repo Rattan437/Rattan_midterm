1cabac06c1566170343447faecb93e9fc797b57f
saval <saval@RAMDEVKRUPA>
2024-10-28 10:12:26 -0400

    Added new functionality

---
+++ src/BookAvailability/BookAvailability.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BookAvailability;
+
+/**
+ *
+ * @author saval
+ */
+public class BookAvailability {
+
+  
+    public boolean isBookAvailable(String bookId) {
+        return true; 
+    }
+}
